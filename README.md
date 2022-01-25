82bd7e1010336a411d33f9bae0563b510199088a
Ross Payne <rosspayn3@gmail.com>
2022-01-25 03:32:06 -0600

    added specific product trackers

--- README.md
+++ README.md
@@ -7,13 +7,18 @@ Scripts that use BeautifulSoup to scrape Newegg for a given product that's in st
 
 ## Usage
 Modify `config.py` as you want. It contains two variables: 
-- `sleepDuration` (integer): The number of seconds to wait between checks. Setting this to a higher number may reduce the chance you get flagged (and possibly IP banned).
-- `notificationSoundFile` (string): An absolute path to an audio file to be played when the script finds a product. (included is `Discovery.ogg`, which is the file I used)
+- (integer) `sleepDuration` : The number of seconds to wait between checks. Setting this to a higher number may reduce the chance you get flagged (and possibly IP banned).
+- (string) `notificationSoundFile` : An absolute path to an audio file to be played when the script finds a product. (included is `Discovery.ogg`, which is the file I used)
 
 #### For `newegg-generic.py`
 1. Create a new filtered search on Newegg for your desired product *. Copy this URL to be passed to the script.
 2. Start the script and pass a price limit and the filtered URL ***in quotations*** as command line arguments.
 
-`python3 newegg-generic.py 500 "https://www.newegg.com/p/pl?N=100007709%204131%20601359415&PageSize=96&Order=1"`
+`python3 newegg-generic.py 600 "https://www.newegg.com/p/pl?N=100007709%204131%20601359415&PageSize=96&Order=1"`
 
 **Note: if a price filter is set on the Newegg URL, it must be higher than the amount passed to the script or no results will ever be returned.*
+
+#### For specific trackers:
+1. Start the script and pass an optional price limit as a command line argument. The scripts contain "reasonable" defaults by today's market values.
+
+`python3 newegg-3060-ti.py [999]`
---
+++ newegg-3070.py
@@ -0,0 +1,83 @@
+import sys
+import requests, lxml
+from notifypy import Notify
+from playsound import playsound
+from time import sleep
+from bs4 import BeautifulSoup
+from threading import Thread
+
+
+if len(sys.argv) == 2:
+    try:
+        priceLimit = int(sys.argv[1])
+    except:
+        sys.exit(f"\n🛑 Something went wrong. Correct usage is:\npython3 {sys.argv[0]} [integer price limit]\n")
+elif len(sys.argv) == 1:
+    priceLimit = 750
+else:
+    sys.exit("\n🛑 Incorrect usage. Correct usage is:\n'python3 {sys.argv[0]} [integer price limit]'\n")
+
+print(f"\n🔵 Checking Newegg for RTX 3070 graphics cards with a price limit of ${priceLimit}.\nPress CTRL + C to exit.\n")
+
+numLoops = 0
+numDealsSeen = 0
+sleepDuration = 15
+notificationSound = '/home/ross/Sounds/Notifications/Material Adventures/Discovery.ogg'
+url = 'https://www.newegg.com/p/pl?N=100007709%20601357250%204131&PageSize=96&Order=1'
+
+
+
+def getItems():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+    containers = soup.find_all("div", class_="item-container")
+    return containers
+
+def checkPrices(items):
+    deals = []
+    for item in items:
+        # get price from item
+        stringPrice = item.find("li", class_="price-current").find("strong").text
+        # remove comma
+        intPrice = stringPrice.replace(",", "")
+        intPrice = int(intPrice)
+        # check if price less than limit
+        if(intPrice <= priceLimit):
+            # add custom item to deals found
+            nameTag = item.find("div", class_="item-info").find("a", class_="item-title")
+            itemName = nameTag.text
+            itemLink = nameTag.get("href")
+            dealItem = {
+                "price": "$" + stringPrice,
+                "name": itemName,
+                "href": itemLink
+            }
+            deals.append(dealItem)
+    return deals
+
+def notify():
+    notification = Notify()
+    notification.title = "Found 3070 in stock!"
+    notification.message = "Check your terminal!"
+    notification.send()
+    playsound(notificationSound)
+
+while True:
+    try:
+        items = getItems()
+        deals = checkPrices(items)
+        if len(deals) > 0:
+            numDealsSeen += len(deals)
+            for deal in deals:
+                print("💵 " + deal["price"])
+                # title of product up to 70 chars
+                print("🎮 " + deal["name"][:75] )
+                print("🔗 " + deal["href"] + "\n")
+            thread = Thread(target=notify, daemon=True)
+            thread.start()
+        numLoops += 1
+        print(f"\n============ Total checks: {numLoops}  |  Total 3070 deals seen: {numDealsSeen} ============")
+        print(f"                        Press CTRL + C to exit.")
+        sleep(sleepDuration)
+    except KeyboardInterrupt:
+        sys.exit("\n🛑 Price checker killed.")
