759cf4b2c2232449137ac497a9dd918a463ec0df
Ross Payne <rosspayn3@gmail.com>
2022-01-25 03:00:21 -0600

    update README and other stuff

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # newegg-tracker
-Scripts that use BeautifulSoup to scrape Newegg for a given product that's in stock and below a price threshold.
-
-### Usage
-Start a given script and pass a price as a command line argument to be used as an upper bound for alerts.
-
-`python3 newegg-3060-ti.py 500`
+A script that uses BeautifulSoup to scrape Newegg for a given product that's in stock and below a price threshold
---
+++ newegg-generic.py
@@ -0,0 +1,88 @@
+import sys
+import requests, lxml
+from notifypy import Notify
+from playsound import playsound
+from time import sleep
+from bs4 import BeautifulSoup
+from threading import Thread
+# import config
+
+if len(sys.argv) == 3:
+    try:
+        priceLimit = int(sys.argv[1])
+        url = sys.argv[2]
+    except:
+        sys.exit(f"\n🛑 Something went wrong. Correct usage is:\npython3 {sys.argv[0]} [integer price limit] [string Newegg URL]\n")
+elif len(sys.argv) == 2:
+    try:
+        priceLimit = int(sys.argv[1])
+    except:
+        sys.exit(f"\n🛑 Something went wrong. Correct usage is:\npython3 {sys.argv[0]} [integer price limit]\n")
+else:
+    sys.exit("\n🛑 Incorrect usage. Correct usage is:\n'python3 {sys.argv[0]} [integer price limit] [filtered Newegg URL]'\n")
+
+print(f"\n🔵 Checking Newegg for products with a price limit of ${priceLimit}...\n   Press CTRL + C to exit.\n")
+
+numLoops = 0
+numDealsSeen = 0
+sleepDuration = 15
+notificationSoundFile = '/home/ross/Sounds/Notifications/Material Adventures/Discovery.ogg'
+#url = 'https://www.newegg.com/p/pl?N=100007709%204131%20601359415&PageSize=96&Order=1'
+
+
+
+def getItems():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+    containers = soup.find_all("div", class_="item-container")
+    return containers
+
+def checkPrices(items):
+    deals = []
+    for item in items:
+        # get price from item
+        stringPrice = item.find("li", class_="price-current").find("strong").text
+        # remove comma
+        intPrice = stringPrice.replace(",", "")
+        intPrice = int(intPrice)
+        # check if price less than limit
+        if(intPrice <= priceLimit):
+            # add custom item to deals found
+            nameTag = item.find("div", class_="item-info").find("a", class_="item-title")
+            itemName = nameTag.text
+            itemLink = nameTag.get("href")
+            dealItem = {
+                "price": "$" + stringPrice,
+                "name": itemName,
+                "href": itemLink
+            }
+            deals.append(dealItem)
+    return deals
+
+def notify():
+    notification = Notify()
+    notification.title = "Found product in stock!"
+    notification.message = "Check your terminal!"
+    notification.send()
+    playsound(notificationSoundFile)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            items = getItems()
+            deals = checkPrices(items)
+            if len(deals) > 0:
+                numDealsSeen += len(deals)
+                for deal in deals:
+                    print("💵 " + deal["price"])
+                    # title of product up to 70 chars
+                    print("🎮 " + deal["name"][:75] )
+                    print("🔗 " + deal["href"] + "\n")
+                thread = Thread(target=notify, daemon=True)
+                thread.start()
+            numLoops += 1
+            print(f"\n============ Total checks: {numLoops}  |  Total product deals seen: {numDealsSeen} ============")
+            print(f"                          Press CTRL + C to exit.\n")
+            sleep(sleepDuration)
+        except KeyboardInterrupt:
+            sys.exit("\n🛑 Price checker killed.")
