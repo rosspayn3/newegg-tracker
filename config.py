8f6977849277a59b1ac19790247b70444ff3cf60
Ross Payne <rosspayn3@gmail.com>
2022-01-26 03:13:49 -0600

    changed audio file to .mp3 instead of .ogg due to Windows is bad

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 thisDir = os.path.dirname(os.path.realpath(__file__))
 sleepDuration = 15
-notificationSoundFile = os.path.join(thisDir,'Discovery.ogg')
+notificationSoundFile = os.path.join(thisDir, 'Discovery.mp3')
